c97b74f9f49b7c6a1fd20550402a93590437d565
James-Thorson <JamesT.esq@gmail.com>
2015-05-14 10:59:52 -0700

    small changes to plotting function

--- R/PlotImage_Fn.R
+++ R/PlotImage_Fn.R
@@ -1,7 +1,7 @@
-PlotImage_Fn <-
-function( Vec, breaks, Xset, Yset, DFexpand, ...){
+PlotImage_Fn <-                 
+function( Vec, breaks, Xset, Yset, loc_to_domain, ...){
   X = c(Xset[1]-mean(diff(Xset))/2,Xset+mean(diff(Xset))/2)
   Y = c(Yset[1]-mean(diff(Yset))/2,Yset+mean(diff(Yset))/2)
-  Z = matrix(Breaks_Fn(Vec[DFexpand[,'r_loc']],breaks=breaks),ncol=length(Xset))
+  Z = matrix(Breaks_Fn(Vec[loc_to_domain],breaks=breaks),ncol=length(Xset))
   image( z=Z, x=X, y=Y, breaks=1:length(breaks), col=Col(length(breaks)-1), ...)
 }
