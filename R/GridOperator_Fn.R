537b604fc266ec4d8def32f9ccf2f5b350f8c4d6
James-Thorson <JamesT.esq@gmail.com>
2015-05-14 08:57:28 -0700

    implementing validated solution

--- R/GridOperator_Fn.R
+++ R/GridOperator_Fn.R
@@ -19,14 +19,14 @@ function( grid_dataframe ){
   gradx_lwr[upper.tri(gradx,diag=TRUE)] = 0
   gradx_upr[lower.tri(gradx,diag=TRUE)] = 0
   diag(gradx_upr) = -colSums( as.matrix(gradx_upr) )
-  #diag(gradx_lwr) = -colSums(gradx_lwr)
+  diag(gradx_lwr) = -colSums( as.matrix(gradx_lwr) )
   
   ## grady -- left and right -- zero flux across boundary
   grady_lwr = grady_upr = grady
   grady_lwr[upper.tri(grady,diag=TRUE)] = 0
   grady_upr[lower.tri(grady,diag=TRUE)] = 0
-  #diag(grady_upr) = -colSums(grady_upr)
-  #diag(grady_lwr) = -colSums(grady_lwr)
+  diag(grady_upr) = -colSums( as.matrix(grady_upr) )
+  diag(grady_lwr) = -colSums( as.matrix(grady_lwr) )
 
   # Return objects
   Return = list("nablaxx"=nablaxx, "nablayy"=nablayy, "nablaxy"=nablaxy, "gradx"=gradx, "grady"=grady, "gradx_lwr"=gradx_lwr, "gradx_upr"=gradx_upr, "grady_lwr"=grady_lwr, "grady_upr"=grady_upr )
