30f8639588fd680eaab46a701e0913c0a4495f91
James-Thorson <JamesT.esq@gmail.com>
2015-05-28 12:48:18 -0700

    small changes to simulator

--- R/SpatProdSimulator_Fn.R
+++ R/SpatProdSimulator_Fn.R
@@ -35,21 +35,24 @@ SpatProdSimulator_Fn = function( MoveMat, SD_omega=1, SD_epsilon=1, SD_effort=1,
       # Production
       if( Dynamical_Model=="Gompertz" ) u_rt[,t] = upred_rt[,t] * exp(alpha + Omega_r - beta*log(upred_rt[,t]/km2_r) + Epsilon_rt[,t])
       if( Dynamical_Model=="Ricker" ) u_rt[,t] = upred_rt[,t] * exp(alpha + Omega_r - beta*(upred_rt[,t]/km2_r) + Epsilon_rt[,t])
+      #if( Dynamical_Model=="Gompertz" ) u_rt[,t] = km2_r * (upred_rt[,t]/km2_r) * exp(alpha + Omega_r - beta*log(upred_rt[,t]/km2_r) + Epsilon_rt[,t]) )
+      #if( Dynamical_Model=="Ricker" ) u_rt[,t] = km2_r * (upred_rt[,t]/km2_r) * exp(alpha + Omega_r - beta*(upred_rt[,t]/km2_r) + Epsilon_rt[,t]) )
     }
   }
 
   # Simulate samples for each site and year
   DF = expand.grid("s_i"=1:n_s, "t_i"=1:n_t)
   DF = cbind( DF, "r_i"=r_s[DF[,'s_i']], "km2_i"=1 )
-  DF = cbind( DF, "cexp_i"=u_rt[ as.matrix(DF[,c('r_i','t_i')]) ] / km2_r[DF[,'r_i']] * DF[,'km2_i'] )
+  DF = cbind( DF, "cexp_i"=u_rt[ as.matrix(DF[,c('r_i','t_i')]) ] * DF[,'km2_i']/km2_r[DF[,'r_i']] )
   DF = cbind( DF, "c_i"=rpois(n_s*n_t, lambda=DF[,'cexp_i']) )
   #DF = cbind( DF, "cpue_i"=NA)
   #for(i in 1:nrow(DF)) DF[i,'cpue_i'] = sum(rlnorm(DF[,'c_i'], meanlog=log(sizepar[1])-sizepar[2]^2/2, sdlog=sizepar[2]))
   #cpue_mean_i = sizepar[1] * DF[,'c_i']
   #cpue_CV_i = sizepar[2]/sizepar[1] * DF[,'c_i']^(-0.5)
   #DF = cbind( DF, "cpue_i"=rgamma(nrow(DF), shape=cpue_CV_i^(-2), scale=cpue_mean_i*cpue_CV_i^2) )      # ~ gamma( shape= CV^(-2), scale=mean*CV^2 )
-  DF = cbind( DF, "zinflognorm_i"=ifelse(DF[,'c_i']>0,1,0) * rlnorm(nrow(DF), meanlog=log(DF[,'cexp_i']), sdlog=1) )
-  DF = cbind( DF, "zinfgamma_i"=ifelse(DF[,'c_i']>0,1,0) * rgamma(nrow(DF), shape=1^(-2), scale=DF[,'cexp_i']*1^2) )
+  DF = cbind( DF, "encounterprob_i"=1-exp(-DF[,'cexp_i']) )
+  DF = cbind( DF, "zinflognorm_i"=ifelse(DF[,'c_i']>0,1,0) * rlnorm(nrow(DF), meanlog=log(DF[,'cexp_i']/DF[,'encounterprob_i']), sdlog=1) )
+  DF = cbind( DF, "zinfgamma_i"=ifelse(DF[,'c_i']>0,1,0) * rgamma(nrow(DF), shape=1^(-2), scale=DF[,'cexp_i']/DF[,'encounterprob_i']*1^2) )
 
   # Total catches
   catch_t = colSums( catch_rt )
